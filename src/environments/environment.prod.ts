91eaf99b02a22e40ee94ca6976059935628f01c0
Unknown <hardmattlake@hotmail.com>
2018-09-12 13:06:29 -0500

    environment arreglo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
-    baseUrl: 'http://localhost:4200',
-    apiUrl: 'http://localhost/sgd_backend_v_2_1/public/api',
+    baseUrl: 'http://144.217.89.76/sgd/app',
+    apiUrl: 'http://144.217.89.76/sgd/api/public/api',
     auth: {
-        urlToken: 'http://localhost/sgd_backend_v_2_1/public/oauth/token',
+        urlToken: 'http://144.217.89.76/sgd/api/public/oauth/token',
         secret: 'GzjgA5FBjRtF270LjKN9WUTRojaTg6v0uCN37Dfq',
         clientId: 2
     },
