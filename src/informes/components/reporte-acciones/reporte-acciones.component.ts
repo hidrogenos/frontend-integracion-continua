5026c83b712236f28374a8d2944201a78a8ede05
Unknown <hardmattlake@hotmail.com>
2018-10-02 08:28:51 -0500

    Corrección componente reportes acciones
    Corrección componente reportes acciones

--- src/informes/components/reporte-acciones/reporte-acciones.component.ts
+++ src/informes/components/reporte-acciones/reporte-acciones.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Output } from "@angular/core";
         label="descargar reporte desviación" (click)="exportarReporteDesviacion()"></button>
     `
 })
-export class ReporteDesviacionComponent {
+export class ReporteAccionesComponent {
 
     @Output()
     onExportarReporteDesviacion = new EventEmitter();
