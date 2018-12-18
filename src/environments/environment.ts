ec1e05c9f78762e677faa947b588880ebadcd6e3
Unknown <j.penuela.e@hotmail.com>
2018-12-18 11:22:40 -0500

    Enviromente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
     production: true,
-    baseUrl: 'http://localhost:4200',
-    apiUrl: 'http://localhost/sgd_backend_v_2_1/public/api',
+    baseUrl: 'http://144.217.89.76/sgd/app',
+    apiUrl: 'http://144.217.89.76/sgd/api/public/api',
     auth: {
         urlToken: 'http://144.217.89.76/sgd/api/public/oauth/token',
         secret: 'GzjgA5FBjRtF270LjKN9WUTRojaTg6v0uCN37Dfq',
